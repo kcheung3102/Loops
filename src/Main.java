e6ff176d01422d41a4251c2af7d3b02dba1105c4
kcheung3102 <kcheung3102@gmail.com>
2019-10-22 16:57:45 -0400

    commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+
+    public static void main(String[] args) {
+	 for(int i = 1; i <= 10; i++){
+	     System.out.println(i);
+     }
+
+	 for(int i = 10; i >= 1;i--) {
+	     System.out.println(i);
+     }
+
+	 for( int i = 1;i <=20;i= i + 2) {
+	     System.out.println(i);
+     }
+
+	 for(int i = 2; i <= 20; i = i + 2) {
+	     System.out.println(i);
+     }
+    }
+}
